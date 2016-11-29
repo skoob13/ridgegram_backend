a6f1a2b3e72e750f56519175d708c53617549089
skoober <skoob17@mail.ru>
2016-11-29 09:32:04 +0500

    Revert project restructuer

--- api/config/env/production.js
+++
@@ -1,7 +0,0 @@
-export default {
-  env: 'production',
-  jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-  jwtAudience: 'ridgegram.com',
-  db: 'mongodb://localhost:27017/ridgegram_prod',
-  port: 3000
-};
